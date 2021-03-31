4e4ca24a3245918a86e36874a7dfd6b379606ba8
Seyfi <by.seyfi@gmail.com>
2021-03-31 23:18:58 +0300

    Initial commit

---
+++ src/Contracts/Pointable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace NeonTurk\Pointable\Contracts;
+
+use Illuminate\Database\Eloquent\Model;
+
+interface Pointable
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function transactions();
+
+    /**
+     *
+     * @return mix
+     */
+    public function countTransactions();
+
+
+    /**
+     * @param $amount
+     * @param $message
+     * @param $data
+     *
+     * @return static
+     */
+    public function addPoints($amount, $message, $data = null);
+    public function subPoints($amount, $message, $data = null);
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,162 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '08cf2c16f9098fbf7ed617bcf7ba254be31862be',
+    'name' => 'neonturk/laravel-pointable',
+  ),
+  'versions' => 
+  array (
+    'doctrine/inflector' => 
+    array (
+      'pretty_version' => '2.1.x-dev',
+      'version' => '2.1.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6a0da4f0e06aa5cd83a2c1a4e449fae98c8bad7',
+    ),
+    'illuminate/collections' => 
+    array (
+      'pretty_version' => '8.x-dev',
+      'version' => '8.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3313bb9066ad60d91a3407b3ef02c114e2ecf8eb',
+    ),
+    'illuminate/contracts' => 
+    array (
+      'pretty_version' => '8.x-dev',
+      'version' => '8.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9c7a9868d7485a82663d67109429094c8e4ed56d',
+    ),
+    'illuminate/macroable' => 
+    array (
+      'pretty_version' => '8.x-dev',
+      'version' => '8.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '300aa13c086f25116b5f3cde3ca54ff5c822fb05',
+    ),
+    'illuminate/support' => 
+    array (
+      'pretty_version' => '8.x-dev',
+      'version' => '8.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f59c467c1b612122488a262e7f9fb32b30df36a',
+    ),
+    'neonturk/laravel-pointable' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '08cf2c16f9098fbf7ed617bcf7ba254be31862be',
+    ),
+    'nesbot/carbon' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '2.x-dev',
+      ),
+      'reference' => '2fd2c4a77d58a4e95234c8a61c5df1f157a91bf4',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '381524e8568e07f31d504a945b88556548c8c42e',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.0.x-dev',
+      ),
+      'reference' => '5a7b96b1dda5d957e01bc1bfe77dcca09c5a7474',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f38c8804a9e97d23e0c8d63341088cd8a22d627',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.22.x-dev',
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.22.x-dev',
+      ),
+      'reference' => 'dc3063ba22c2a1fd2f45ed856374d79114998f91',
+    ),
+    'symfony/translation' => 
+    array (
+      'pretty_version' => '5.x-dev',
+      'version' => '5.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '84f3e8172eee0a97ba3506e30233472fb415d8c4',
+    ),
+    'symfony/translation-contracts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '2.4.x-dev',
+      ),
+      'reference' => '502490047e9064e6f1bc331ab1d43ca3da68983d',
+    ),
+    'symfony/translation-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '2.3',
+      ),
+    ),
+    'voku/portable-ascii' => 
+    array (
+      'pretty_version' => '1.5.6',
+      'version' => '1.5.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '80953678b19901e5165c56752d087fc11526017c',
+    ),
+  ),
+);
